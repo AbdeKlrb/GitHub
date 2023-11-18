cda6a9d84c96f22b38a6d4397a6fed1fa902fa05
AbdeLarbi <136172941+AbdeKlrb@users.noreply.github.com>
2023-11-18 18:11:58 +0100

    Update Pandas.py
    Added group column to dataframe, Test Github Pull,Create New Branch

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 names = ["A", "B", "C", "D","C"]
 ages = [15,52,42,16,45]
+group = ["G1", "G1", "G2", "G3", "G1"]
 
-data = {"Names":names , "Ages" : ages}
+data = {"Names":names , "Ages" : ages, "groups" : group}
 
 df = pd.DataFrame(data)
 
