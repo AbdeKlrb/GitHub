f0eebcc7556251fb66b52ae0950fde470f2a88eb
AbdeLarbi <136172941+AbdeKlrb@users.noreply.github.com>
2023-11-24 16:52:26 +0100

    Update Pandas.py

--- Python/Pandas.py
+++ Python/Pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-names = ["A", "B", "C", "D","C"]
+names = ["A", "B", "C", "D","E"]
 ages = [15,52,42,16,45]
 
 data = {"Names":names , "Ages" : ages}
@@ -8,3 +8,5 @@ data = {"Names":names , "Ages" : ages}
 df = pd.DataFrame(data)
 
 print(df.head())
+
+#24.11
